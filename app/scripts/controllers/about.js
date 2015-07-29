b6799efb94a111bb8f2b1be857ebf65b482b9f9d
Dmitry BRANAVITSKI <dima_nado@mail.ru>
2015-07-29 11:11:33 +0300

    new angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dakNewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dakNewApp
+ */
+angular.module('dakNewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dakNewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dakNewApp
+ */
+angular.module('dakNewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
