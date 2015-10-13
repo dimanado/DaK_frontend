7635c4ef0e5c7f14e7878aa2167b6c66afcbb171
Maxim <horokhovich@gmail.com>
2015-10-13 23:59:02 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,21 @@
 var gulp = require('gulp'),
-  webserver = require('gulp-webserver'),
-  connect = require('gulp-connect'),
-  replace       = require('gulp-replace'),
-  rename        = require('gulp-rename'),
-  templateCache = require('gulp-angular-templatecache');
+    connect = require('gulp-connect'),
 
 
-
-function setEndpoints(env) {
-  gulp.src(['./app/scripts/config.js.template'])
-
-    .pipe(replace(/\{\{(api|socket|service)Endpoint\}\}/g, function (match) {
-      return JSON.stringify(endPoints[env][match.replace(/\{|\}/g, '')]);
-    }))
-    .pipe(rename('./app/scripts/config.js'))
-    .pipe(gulp.dest('.'));
-}
-
-gulp.task('webserver', function() {
-  gulp.src('app')
-    .pipe(webserver({
-      host: 'localhost',
-      port: 9000,
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('./app/**/*', function() {
-    gulp.src('./app/**/*').pipe(connect.reload());
+gulp.task('server', function() {
+  connect.server({
+    root: ['app', '.'],
+    livereload: true
   });
 });
 
-gulp.task('set_develop_endpoints', function() {
-  setEndpoints('development');
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/views/*.html'], ['html']);
 });
 
-gulp.task('buildTemplates', function() {
-  var options = {
-    output: 'app/scripts',
-    strip: 'app/views',
-    moduleName: 'templates'
-  };
-
-  gulp.src('app/views/*.html')
-    .pipe(templateCache(options))
-    .pipe(gulp.dest(options.output));
-});
-
-//gulp.task('default', ['webserver', 'watch']);
- gulp.task('default', ['set_develop_endpoints', 'buildTemplates', 'watch', 'webserver']);
+gulp.task('serve', ['server', 'watch']);
