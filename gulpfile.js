960f5151f88e7e0c31ca6cffef278455606eb12b
Dmitry BRANAVITSKI <dima_nado@mail.ru>
2015-10-14 23:31:25 +0300

    add logik work with scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('server', function() {
   connect.server({
     root: ['app', '.'],
+    port: 9000,
     livereload: true
   });
 });
@@ -13,9 +16,19 @@ gulp.task('html', function () {
   gulp.src('./app/*.html')
     .pipe(connect.reload());
 });
- 
-gulp.task('watch', function () {
-  gulp.watch(['./app/views/*.html'], ['html']);
+
+//сделать группировку всех scss в main.css
+gulp.task('scss', function () {
+  gulp.src('./app/styles/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./app/styles/css'));
 });
 
-gulp.task('serve', ['server', 'watch']);
+gulp.task('watch', function () {
+  gulp.watch(['./app/views/*.html'], ['html']);
+  gulp.watch('./sass/**/*.scss', ['scss']);
+});
+
+gulp.task('serve', ['scss', 'server', 'watch']);
